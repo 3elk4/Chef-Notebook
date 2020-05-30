816e40c43eebd68ebd4105578a5e30acd8dc8dd5
belka <izaa1109@gmail.com>
2020-05-30 16:55:19 +0200

    view - recipe manager
    project properties
    main app + xaml

---
+++ Chef's notebook/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Chefs_notebook;
+using Chefs_notebook.model;
+using Chefs_notebook.view;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chef_s_notebook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RecipeManager RecipeManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Recipe recipe1 = RecipeBuilder.Builder()
+                .withName("Test1")
+                .withCategories(Category.BREAKFAST, Category.DRINK)
+                .withIngredients(IngredientBuilder.Builder()
+                    .WithName("JAJCA")
+                    .WithQuantity(2)
+                    .WithUnit(Unit.PIECE)
+                    .Build(), IngredientBuilder.Builder()
+                    .WithName("Mleko")
+                    .WithQuantity(2)
+                    .WithUnit(Unit.GLASS)
+                    .Build())
+                .withPortions(2)
+                .build();
+
+            Recipe recipe2 = RecipeBuilder.Builder()
+                .withName("Test2")
+                .withCategories(Category.LUNCH, Category.VEGETARIAN)
+                .withIngredients(IngredientBuilder.Builder()
+                    .WithName("JAJCA")
+                    .WithQuantity(2)
+                    .WithUnit(Unit.PIECE)
+                    .Build(), IngredientBuilder.Builder()
+                    .WithName("Mleko")
+                    .WithQuantity(2)
+                    .WithUnit(Unit.GLASS)
+                    .Build())
+                .withPortions(2)
+                .build();
+
+
+            RecipeManager = new RecipeManager(this);
+            RecipeManager.Recipes.Add(recipe1);
+            RecipeManager.Recipes.Add(recipe2);
+            this.Categories.ItemsSource = Enum.GetValues(typeof(Category)).Cast<Category>();
+        }
+
+        private void Categories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Category category = ExtractCategory(sender);
+            this.Recipes.ItemsSource = RecipeManager.FilterByCategory(category);
+        }
+
+        private void Recipes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Recipe recipe = ExtractRecipe(sender);
+            RecipeManager.ShowRecipe(recipe);
+        }
+
+        private Category ExtractCategory(object combobox)
+        {
+            string categoryName = (combobox as ComboBox).SelectedItem.ToString();
+            return (Category)Enum.Parse(typeof(Category), categoryName);
+        }
+
+        private Recipe ExtractRecipe(object listbox)
+        {
+            if ((listbox as ListBox).SelectedItem == null) return null;
+            string recipeName = (listbox as ListBox).SelectedItem.ToString();
+            return RecipeManager.FindByName(recipeName);
+        }
+    }
+}
---
+++ Chef's notebook/view/RecipeManager.cs
@@ -0,0 +1,67 @@
+﻿using Chef_s_notebook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chefs_notebook.view
+{
+    class RecipeManager
+    {
+        private const String CATEGORY_FORMAT = "{0}, ";
+        private const String INGREDIENT_FORMAT = "   > {0}\n";
+
+        private MainWindow mainWindow;
+        public List<Recipe> Recipes { get; set; } // temporary
+
+        public RecipeManager(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            Recipes = new List<Recipe>();
+        }
+
+        public IEnumerable<string> FilterByCategory(Category category)
+        {
+            return Recipes
+                .FindAll(recipe => recipe.Categories.Contains(category))
+                .Select(recipe => recipe.Name);
+        }
+
+        public Recipe FindByName(string name)
+        {
+            return Recipes.Find(recipe => recipe.Name.Equals(name));
+        }
+
+        public void ShowRecipe(Recipe recipe)
+        {
+            if (recipe == null) return;
+            mainWindow.RecipeName.Text = recipe.Name;
+            mainWindow.RecipePortion.Text = recipe.Portions.ToString();
+            mainWindow.RecipePreparation.Text = recipe.Preparation;
+            mainWindow.RecipeNotes.Text = recipe.Notes;
+            mainWindow.RecipeSources.Text = recipe.Sources;
+            mainWindow.RecipeCategory.Text = ConvertCategories(CATEGORY_FORMAT, recipe.Categories);
+            mainWindow.RecipeIngredients.Text = ConvertIngredients(INGREDIENT_FORMAT, recipe.Ingredients);
+        }
+
+        private string ConvertCategories(string format, List<Category> items)
+        {
+            StringBuilder result = new StringBuilder();
+            items.ForEach(item => result.AppendFormat(format, item.ToString()));
+            return result.ToString();
+        }
+
+        private string ConvertIngredients(string format, List<Ingredient> items)
+        {
+            StringBuilder result = new StringBuilder();
+            items.ForEach(item => result.AppendFormat(format, item.ToString()));
+            return result.ToString();
+        }
+
+        private void SaveRecipe()
+        {
+
+        }
+    }
+}
